beace28ca28d59fe52a7b423b23f67f75ddb7399
Byte <deadcardinal293@gmail.com>
2023-08-22 12:13:43 +0700

    Added: exceptions

--- temp_mail/temp_mail.py
+++ temp_mail/temp_mail.py
@@ -3,26 +3,41 @@ import json
 import requests
 
 from typing import List, Dict, Union
-from temp_mail.types.mail_content import MailContent
-from temp_mail.types.mail_response import MailResponse
+from .types.mail_content import MailContent
+from .types.mail_response import MailResponse
+from .mail_exceptions import APIError, TimeoutError
 
 
-class TenMinuteMail:
+class TempMail:
     base_url = "https://10minutemail.net/"
     
     def __init__(self):
         self._session = requests.Session()
 
     def _response(self) -> MailResponse | None:
-        json_data = json.loads(self._session.get(self.base_url + "address.api.php").text)
-        permalink = {key: json_data["permalink"][key] for key in json_data["permalink"].keys()}
-        mail_list = [{key: item[key] for key in item.keys()} for item in json_data["mail_list"]]
+        response = self._session.get(self.base_url + "address.api.php")
+        if response.status_code != 200:
+            raise APIError(response.status_code, "Failed to get response data")
+        
+        json_data = json.loads(response.text)
+        permalink = {
+            key: json_data["permalink"][key] 
+            for key in json_data["permalink"].keys()
+        }
+        mail_list = [
+            {key: item[key] for key in item.keys()} 
+            for item in json_data["mail_list"]
+        ]
         data = {key: json_data[key] for key in json_data.keys()}
         data.update(permalink=permalink, mail_list=mail_list)
         return MailResponse(**data)
 
     def _content(self, params: Dict[str, str]) -> MailContent | None:
-        json_data = json.loads(self._session.get(self.base_url + "mail.api.php", params=params).text)
+        response = self._session.get(self.base_url + "mail.api.php", params=params)
+        if response.status_code != 200:
+            raise APIError(response.status_code, "Failed to get content data")
+        
+        json_data = json.loads(response.text)
         json_data["from_"] = json_data["from"]
         return MailContent(**json_data)
         
@@ -30,7 +45,10 @@ class TenMinuteMail:
         return self._response()
     
     def get_mailbox(self) -> List[Union[str, None]]:
-        return [self.get_message(mail_info.mail_id).body for mail_info in self._response().mail_list]
+        return [
+            self.get_message(mail_info.mail_id).body 
+            for mail_info in self._response().mail_list
+        ]
     
     def get_message(self, mail_id: str) -> MailContent | None:
         return self._content(params={"mailid": mail_id})
@@ -46,11 +64,11 @@ class TenMinuteMail:
         timeout_time = 1
         while timeout > timeout_time:
             email = self.get_last_email()
-            if email:
+            if email: 
                 return email.body[0].body
             timeout_time += 1
             time.sleep(1)
-        print("Timed out waiting for message")
+        TimeoutError("Timed out waiting for message")
     
     def get_email_address(self) -> str:
         return self._response().mail_get_mail
