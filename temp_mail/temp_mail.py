17d24a0440e9c2b914c03ac9e34a6e7933dc40b4
Byte <deadcardinal293@gmail.com>
2023-08-22 10:03:27 +0700

    Minor edits

--- temp_mail/temp_mail.py
+++ temp_mail/temp_mail.py
@@ -2,49 +2,47 @@ import time
 import json
 import requests
 
-from typing import List, Dict
+from typing import List, Dict, Union
 from temp_mail.types.mail_content import MailContent
-from temp_mail.types.mail_response import MailResponse, MailResponseMailList
+from temp_mail.types.mail_response import MailResponse
 
 
 class TenMinuteMail:
+    base_url = "https://10minutemail.net/"
+    
     def __init__(self):
         self._session = requests.Session()
 
     def _response(self) -> MailResponse | None:
-        url = "https://10minutemail.net/address.api.php"
-        response = self._session.get(url)
-        json_data = json.loads(response.text)
-        json_data["mail_list"] = [
-            MailResponseMailList(
-                mail_id=item["mail_id"],
-                from_=item["from"],
-                subject=item["subject"],
-                datetime=item["datetime"],
-                datetime2=item["datetime2"],
-                timeago=item["timeago"],
-                isread=item["isread"]
-            )
-            for item in json_data["mail_list"]    
-        ]
-        try:
-            return MailResponse(**json_data)
-        except Exception as ex:
-            print(ex)
-            return None
+        json_data = json.loads(self._session.get(self.base_url + "address.api.php").text)
+        permalink = {key: json_data["permalink"][key] for key in json_data["permalink"].keys()}
+        mail_list = [{key: item[key] for key in item.keys()} for item in json_data["mail_list"]]
+        data = {key: json_data[key] for key in json_data.keys()}
+        data.update(permalink=permalink, mail_list=mail_list)
+        return MailResponse(**data)
 
     def _content(self, params: Dict[str, str]) -> MailContent | None:
-        url = "https://10minutemail.net/mail.api.php"
-        response = self._session.get(url, params=params)
-        json_data = json.loads(response.text)
+        json_data = json.loads(self._session.get(self.base_url + "mail.api.php", params=params).text)
         json_data["from_"] = json_data["from"]
-        try:
-            return MailContent(**json_data)
-        except Exception as ex:
-            print(ex)
+        return MailContent(**json_data)
+        
+    def get_email(self) -> MailResponse | None:
+        return self._response()
+    
+    def get_mailbox(self) -> List[Union[str, None]]:
+        return [self.get_message(mail_info.mail_id).body for mail_info in self._response().mail_list]
+    
+    def get_message(self, mail_id: str) -> MailContent | None:
+        return self._content(params={"mailid": mail_id})
+    
+    def get_last_message(self) -> MailContent | None:
+        mail_id = self._response().mail_list[0].mail_id
+        if mail_id != "welcome":
+            return self.get_message(mail_id)
+        else:
             return None
     
-    def wait_for_message(self, timeout: int = 10):
+    def wait_for_message(self, timeout: int = 10) -> str:
         timeout_time = 1
         while timeout > timeout_time:
             email = self.get_last_email()
@@ -53,37 +51,21 @@ class TenMinuteMail:
             timeout_time += 1
             time.sleep(1)
         print("Timed out waiting for message")
-        
-    def get_email_address(self) -> MailResponse | None:
-        response = self._response()
-        return response
     
-    def get_email_content(self, mail_id: str) -> MailContent | None:
-        response = self._content(params={"mailid": mail_id})
-        return response
-
-    def get_all_email_content(self) -> List[str]:
-        response = self._response()
-        return [self.get_email_content(mail_info.mail_id).body for mail_info in response.mail_list]
-
-    def get_last_email(self) -> MailContent:
-        response = self._response()
-        mail_id = response.mail_list[0].mail_id
-        if mail_id != "welcome":
-            return self.get_email_content(mail_id)
+    def get_email_address(self) -> str:
+        return self._response().mail_get_mail
+    
+    def get_left_time(self) -> int:
+        return self._response().mail_left_time
     
     def reset_10_minutes(self) -> None:
-        url = "https://10minutemail.net/more.html"
-        self._session.get(url)
+        self._session.get(self.base_url + "more.html")
 
     def reset_100_minutes(self) -> None:
-        url = "https://10minutemail.net/more100.html"
-        self._session.get(url)
+        self._session.get(self.base_url + "more100.html")
 
     def generate_new_email_address(self) -> None:
-        url = "https://10minutemail.net/new.html"
-        self._session.get(url)
+        self._session.get(self.base_url + "new.html")
 
     def recover_email_address(self) -> None:
-        url = "https://10minutemail.net/recover.html"
-        self._session.get(url)
+        self._session.get(self.base_url + "recover.html")
